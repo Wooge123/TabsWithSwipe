c2fed7c6a0a6db0ad5937b8f87870aee5796de0d
fuyukang <308515050@qq.com>
2016-11-01 15:22:21 +0800

    First Commit

---
+++ src/com/yukang/tabswithswipe/adapter/TabsPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.yukang.tabswithswipe.adapter;
+
+import com.yukang.tabswithswipe.GamesFragment;
+import com.yukang.tabswithswipe.MoviesFramgent;
+import com.yukang.tabswithswipe.TopRatedFragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+	public TabsPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int index) {
+		
+		switch (index) {
+		case 0:
+			// Top Rated fragment activity
+			return new TopRatedFragment();
+		case 1:
+			// Games fragment activity
+			return new GamesFragment();
+		case 2:
+			// Movies fragment activity
+			return new MoviesFramgent();
+		}
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// get item count - equal to number of tabs
+		return 3;
+	}
+
+}
